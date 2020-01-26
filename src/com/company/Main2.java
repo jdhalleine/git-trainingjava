43b3c43d45a4798980458e87d5806be3b4d26561
Jonathan Dhalleine <jonathan.dhalleine@decathlon.com>
2020-01-26 21:01:55 +0100

    remove and add line in Main2.java

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -33,9 +33,8 @@ class Main2{
 
         Function<String, String> f =  s -> s;
         System.out.println("tzst");
-        System.out.println("tzst");
-        System.out.println("tzst");
-        System.out.println("tzst");
+        System.out.println("titi");
+
 
 
 
